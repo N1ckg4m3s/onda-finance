bcf62f9d5b1184ed2f2a87ff9482d2af75f35b46
n1ckg4m3s <nico.ag.03@hotmail.com>
2026-03-28 15:53:45 -0300

    [ REFAC ] Refatorando validateCredentials par ausar a AccountKey e obter a conta

--- src/services/handlers/auth/auth.repository.ts
+++ src/services/handlers/auth/auth.repository.ts
@@ -1,15 +1,17 @@
+import { FAKE_DB } from "../../../infra/db/FAKE_DB";
+import { generateAccountKey } from "../transaction/transaction.helper";
 export const authRepository = {
     validateCredentials: async (account: string, agency: string,) => {
         // banco mock
-        if (account !== "101010" || agency !== "0001") throw new Error('No Account');
+        const accountKey = generateAccountKey({ account, agency })
+
+        const userAccount = FAKE_DB.accounts[accountKey]
+        if (!userAccount) throw new Error('No Account');
 
         //retorno de sucesso
         return {
-            id: "acc_1",
-            agency,
-            account,
-            ownerName: "Mock owner",
-            balance: 1000
+            id: accountKey,
+            ...userAccount
         }
-    }
+    },
 }
