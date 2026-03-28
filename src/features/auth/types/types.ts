290c7c07cb0afe84e163d895f2ce09a2132a1176
n1ckg4m3s <nico.ag.03@hotmail.com>
2026-03-28 15:56:25 -0300

    [ FEAT ] Atualizando tipagem da conta

--- src/features/auth/types/types.ts
+++ src/features/auth/types/types.ts
@@ -1,3 +1,12 @@
+export type Account = {
+    agency: string;
+    account: string;
+    balance: number;
+    owderName: string
+};
+
+export type AccountResume = Omit<Account, 'balance'>
+
 export type AuthSession = {
     token: string,
     account: {
