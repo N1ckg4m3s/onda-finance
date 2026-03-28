b2b36bb372f7e4f636713b8a3226d3e34ad6afc2
n1ckg4m3s <nico.ag.03@hotmail.com>
2026-03-28 15:59:21 -0300

    [ FEAT ] Centralizando FAKE_DB

---
+++ src/infra/db/FAKE_DB.ts
@@ -0,0 +1,28 @@
+import type { Account } from "../../features/auth/types/types";
+import type { Transaction } from "../../features/transaction/types/types";
+
+interface DB_Schema {
+    accounts: Record<string, Account>,
+    transactions: Record<string, Transaction[]>,
+}
+
+export const FAKE_DB: DB_Schema = {
+    accounts: {
+        "101010-0001": {
+            owderName: 'Mock user 2',
+            account: '101010',
+            agency: '0001',
+            balance: 500,
+        },
+        "202020-0001": {
+            owderName: 'Mock user 1',
+            account: '202020',
+            agency: '0001',
+            balance: 500,
+        }
+    },
+    transactions: {
+        "101010-0001": [],
+        "202020-0001": [],
+    }
+};
