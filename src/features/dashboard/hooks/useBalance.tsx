d21a67aed65d156112912382f23ad99c367a2dc2
n1ckg4m3s <nico.ag.03@hotmail.com>
2026-03-30 08:38:15 -0300

    [ REFAC ] Melhorando a queryKey para cache conciente

--- src/features/dashboard/hooks/useBalance.tsx
+++ src/features/dashboard/hooks/useBalance.tsx
@@ -1,13 +1,16 @@
 import { useQuery } from "@tanstack/react-query"
 import { getUserBalance } from "../service/getUserBalance"
+import { useAuthStore } from "../../auth/store/auth.store";
 
 export const useBalance = () => {
+    const session = useAuthStore((state) => state.session);
+
     const {
         data: balance,
         error: balanceError,
         isLoading: balanceLoading
     } = useQuery({
-        queryKey: ['balance'],
+        queryKey: ['balance', session?.account?.account, session?.account?.agency],
         queryFn: getUserBalance
     })
 
--- src/features/transaction/hooks/useCreateTransaction.ts
+++ src/features/transaction/hooks/useCreateTransaction.ts
@@ -1,13 +1,16 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createTransaction } from "../services/createTransaction";
+import { useAuthStore } from "../../auth/store/auth.store";
 
 export const useCreateTransaction = () => {
+    const session = useAuthStore((state) => state.session);
     const queryClient = useQueryClient();
 
     return useMutation({
         mutationFn: createTransaction,
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['transactions'] });
+            queryClient.invalidateQueries({ queryKey: ['balance', session?.account?.account, session?.account?.agency] });
         },
     });
 };
